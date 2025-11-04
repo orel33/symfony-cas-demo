54a1a573401b5d27a78b7b9e682d12dd361f20df
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2025-11-04 12:29:16 +0100

    ajout demo test-cas en PHP pure

--- start-cas-server.sh
+++ start-cas-server.sh
@@ -5,8 +5,9 @@ IMG=apereo/cas:7.3.0
 docker pull $IMG
 
 docker run --rm -it -p 9000:9000 \
-  -v $(pwd)/myfiles/certs/cas-keystore.p12:/etc/cas/ssl/cas-keystore.p12 \
   -v $(pwd)/myfiles/config:/etc/cas/config \
   --name cas-server $IMG
+
+#  -v $(pwd)/myfiles/certs/cas-keystore.p12:/etc/cas/ssl/cas-keystore.p12 \
 
 # docker exec -it cas-server /bin/bash
