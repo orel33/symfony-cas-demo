19aadc5ffffcc7f1508bf766c59605999dd514b2
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2025-11-14 11:13:07 +0100

    fix CAS server base URL in http

--- cas-demo/src/Security/CasAuthenticator.php
+++ cas-demo/src/Security/CasAuthenticator.php
@@ -11,7 +11,8 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use phpCAS;
+
+// use phpCAS;
 
 class CasAuthenticator extends AbstractAuthenticator
 {
@@ -41,29 +42,28 @@ class CasAuthenticator extends AbstractAuthenticator
         $cas_hostname = $_ENV['CAS_SERVER_HOSTNAME'] ?? 'localhost';
         $cas_port = $_ENV['CAS_SERVER_PORT'] ?? '9000';
         $cas_uri = $_ENV['CAS_SERVER_URI'] ?? '/cas';
-
-        // Construire l'URL du serveur CAS
-        // $cas_url = 'http://localhost:9000/cas';
-        $cas_url = "http://$cas_hostname:$cas_port$cas_uri";
+        
+        // Construire l'URL de base du serveur CAS
+        $cas_url = "http://$cas_hostname:$cas_port$cas_uri/"; # don't forget trailing slash!
         error_log('CAS URL: ' . $cas_url);
+        // $cas_url = 'http://localhost:9000/cas/';
 
-        # $service_url = 'http://localhost:8000/hello';     # URL du service (retour après authentification)
-        $service_base_url = 'http://localhost:8000/';            # URL du service (retour après authentification)
-        error_log('phpcas Service BASE URL: ' . $service_base_url);
+        $service_url = 'http://localhost:8000/';  # URL du service (retour après authentification)
         # \phpCAS::client(CAS_VERSION_2_0, 'localhost', 9000, '/cas', $service_url);
-        \phpCAS::client(CAS_VERSION_2_0, $cas_hostname, (int) $cas_port, $cas_uri, $service_base_url, false);
+        \phpCAS::client(CAS_VERSION_2_0, $cas_hostname, (int) $cas_port, $cas_uri, $service_url);
+
+        // \phpCAS::setFixedServiceURL($service_url);
+
+        // FIXME: Dans phpCAS, CAS/Client.php : _getServerBaseURL() force l'URL du serveur CAS en HTTPS au lieu de HTTP dans cette démo.
+        $client = \phpCAS::getCasClient();
+        $client->setBaseURL($cas_url); // overide CAS server base URL with http scheme (to avoid https forced in phpCAS)
+
+        // \phpCAS::setServerLoginURL($cas_url . '/login?service=' . urlencode($service_url));
+        // \phpCAS::setServerServiceValidateURL($cas_url . '/serviceValidate');
+        // \phpCAS::setServerLogoutURL($cas_url . '/logout');
 
         // Désactive la validation du serveur CAS (pour les tests en local)
         \phpCAS::setNoCasServerValidation(); // accepte les certificats auto-signés (test en local uniquement)
-
-        $service_url = \phpCAS::getServiceURL();
-        error_log('[CAS] phpcas service URL: ' . $service_url);
-
-        // Forcer manuellement l'URL de service en HTTP (utile en dev local)
-        \phpCAS::setFixedServiceURL($service_url);
-        \phpCAS::setServerLoginURL($cas_url . '/login?service=' . urlencode($service_url));
-        \phpCAS::setServerServiceValidateURL($cas_url . '/serviceValidate');
-        \phpCAS::setServerLogoutURL($cas_url . '/logout');
 
         // Vérifie si l'utilisateur est authentifié, sinon redirige vers le CAS
         \phpCAS::forceAuthentication();
