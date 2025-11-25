b71d8b47e56e6bf6f58de31eeeac795a78c50d5b
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2025-11-25 19:35:46 +0100

    maj test

--- cas-test/test.php
+++ cas-test/test.php
@@ -7,19 +7,17 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/vendor/autoload.php';
 
 $service = 'https://promo-st.emi.u-bordeaux.fr/'; 
+\phpCAS::setDebug();
 \phpCAS::client(CAS_VERSION_3_0, 'cas.u-bordeaux.fr', 443, '/cas', $service);
 \phpCAS::setNoCasServerValidation(); // accept self-signed certificates (local CAS only)
+// \phpCAS::setCasServerCACert();
 \phpCAS::forceAuthentication();
 
 $user = \phpCAS::getUser();
-$mail = \phpCAS::getAttribute('mail');
-$email = \phpCAS::getAttribute('email');
 
 // Affiche les informations
 echo "<h1>Test CAS</h1>";
 echo "<p>User : " . htmlspecialchars($user) . "</p>";
-echo "<p>Mail : " . htmlspecialchars($mail) . "</p>";
-echo "<p>Email : " . htmlspecialchars($email) . "</p>";
 echo "<p>Attributes :</p>";
 echo "<pre>";
 print_r(\phpCAS::getAttributes());
