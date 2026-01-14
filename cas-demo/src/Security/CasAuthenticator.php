03e7fe7a43c96a67ceb0f743c7f0f5fca4c1c2d1
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2026-01-14 10:46:07 +0100

    update cas-demo to use CAS Ubx

--- cas-demo/src/Security/CasAuthenticator.php
+++ cas-demo/src/Security/CasAuthenticator.php
@@ -11,6 +11,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\InMemoryUser;
 
 // use phpCAS;
 
@@ -36,19 +37,21 @@ class CasAuthenticator extends AbstractAuthenticator
             session_start();
 
 
-        \phpCAS::setDebug('/tmp/phpcas.log');
+        \phpCAS::setDebug('/tmp/phpcas.log'); // FIXME
 
         // cas server configuration
         $cas_hostname = $_ENV['CAS_SERVER_HOSTNAME'] ?? 'localhost';
         $cas_port = $_ENV['CAS_SERVER_PORT'] ?? '9000';
         $cas_uri = $_ENV['CAS_SERVER_URI'] ?? '/cas';
-        $cas_url = "http://$cas_hostname:$cas_port$cas_uri/"; # don't forget trailing slash!
+        // $cas_url = "http://$cas_hostname:$cas_port$cas_uri/"; # don't forget trailing slash!
+	$cas_url = "https://$cas_hostname:$cas_port$cas_uri/"; # don't forget trailing slash!
 
         // service configuration
-        $service_url = 'http://localhost:8000/';
+        // $service_url = 'http://localhost:8000/';
+	$service_url = 'https://promo-st.emi.u-bordeaux.fr/';
         
         // initialize phpCAS
-        \phpCAS::client(CAS_VERSION_2_0, $cas_hostname, (int) $cas_port, $cas_uri, $service_url);     
+        \phpCAS::client(CAS_VERSION_3_0, $cas_hostname, (int) $cas_port, $cas_uri, $service_url);     
         $client = \phpCAS::getCasClient();
         $client->setBaseURL($cas_url);  // for HTTP CAS server (local CAS only)
         \phpCAS::setNoCasServerValidation(); // accept self-signed certificates (local CAS only)
@@ -59,8 +62,12 @@ class CasAuthenticator extends AbstractAuthenticator
         $username = \phpCAS::getUser();
         error_log('[CAS] Utilisateur authentifié : ' . $username);
 
-        $passeport = new SelfValidatingPassport(new UserBadge($username));
-        return $passeport;
+        //$passeport = new SelfValidatingPassport(new UserBadge($username));
+
+	$passeport = new SelfValidatingPassport(
+    	  new UserBadge($username, function ($identifier) { return new InMemoryUser($identifier, null, ['ROLE_USER']); })
+	);
+	return $passeport;
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
